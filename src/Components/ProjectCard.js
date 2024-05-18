9b6ff2a243b178d75eb4902a8ff559f2c13f142d
austinxviii <131591000+austinxviii@users.noreply.github.com>
2024-05-18 16:23:35 +0530

    Link modification-1

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -3,13 +3,15 @@ import { Col } from "react-bootstrap";
 export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
     <Col size={13} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+      <a href="#" className='no-blue'>
+        <div className="proj-imgbx">
+          <img src={imgUrl} />
+          <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
+          </div>
         </div>
-      </div>
+      </a>
     </Col>
   )
 }
