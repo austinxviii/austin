eb6c0f8bf56eeb8afae513048bf04ae80ea4dd38
austinxviii <131591000+austinxviii@users.noreply.github.com>
2024-07-19 18:14:49 +0530

    major changes

--- src/Components/Timeline.js
+++ src/Components/Timeline.js
@@ -2,14 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TimelineWrapper = styled.div`
-  margin: 0 auto;
+  margin: -80px 100px 0 -100px;
   max-width: 750px;
-  padding: 80px 0;
+  padding: 50px 0;
   display: grid;
   grid-template-columns: 1fr 3px 1fr;
   font-family: "Times New Roman", Times, serif;
   color: #ffffff;
-  height: 500px;
+  height: 400px;
   overflow-y: scroll;
   overflow-x: hidden;
   background-color: rgba(17, 17, 17, 0.8);
@@ -30,9 +30,8 @@ const TimelineWrapper = styled.div`
     border-radius: 100px;
   }
 
-   @media (max-width: 600px) {
-    margin-top: -400px;
-    margin-bottom: 0px;
+   @media (max-width: 1100px) {
+    margin: 30px 30px 0 30px;
     height: 450px;
   }
 `;
@@ -44,7 +43,7 @@ const TimelineComponent = styled.div`
   box-shadow: ${({ bg }) => (bg ? '0 0 5px rgba(0, 0, 0, 0.2)' : 'none')};
   border-radius: ${({ bg }) => (bg ? '10px' : '0')};
 
-  @media (max-width: 600px) {
+  @media (max-width: 1100px) {
     padding: 10px 10px 10px 10px;
   }
 `;
@@ -69,7 +68,7 @@ const TimelinePoint = styled.div`
 const TimelineDate = styled.div`
   text-align: ${({ right }) => (right ? 'right' : 'left')};
 
-  @media (max-width: 600px) {
+  @media (max-width: 1100px) {
     margin: -15px;
   }
 `;
@@ -80,15 +79,18 @@ const TimelineTitle = styled.h3`
   font-size: 20px;
   font-weight: bold;
 
-  @media (max-width: 600px) {
+  @media (max-width: 1100px) {
     font-size: 15px;
   }
 `;
 
 const TimelineParagraph = styled.p`
-  line-height: 1.5;
+  text-align: ${({ right }) => (right ? 'right' : 'left')};
+  line-height: 1;
+  margin-left: 0;
+  align-content: left;
   
-  @media (max-width: 600px) {
+  @media (max-width: 1100px) {
     font-size: 11px;
   }
 `;
@@ -97,50 +99,61 @@ const Timeline = () => {
   return (
     <TimelineWrapper>
       <TimelineComponent>
-        <TimelineDate right>June 10, 2024</TimelineDate>
+        <TimelineDate right>July 19, 2024</TimelineDate>
       </TimelineComponent>
       <TimelineMiddle>
         <TimelinePoint />
       </TimelineMiddle>
       <TimelineComponent bg>
-        <TimelineTitle>Continuous Random Variable</TimelineTitle>
+        <TimelineTitle>Interprocess Communication</TimelineTitle>
         <TimelineParagraph>
-          My first YouTube video was a tutorial on how to build a client-server sockets app in Java.
+          <ul>
+            <li>How process communicate in a system</li>
+            <li>And some Leetcode</li>
+          </ul>
         </TimelineParagraph>
       </TimelineComponent>
       <TimelineComponent bg>
-        <TimelineTitle>5,000 Subscribers Q&A</TimelineTitle>
+        <TimelineTitle>Threads</TimelineTitle>
         <TimelineParagraph>
-          To celebrate 5,000 subscribers, I published a video answering some of the most popular questions which my viewers
-          had asked me since starting my YouTube channel.
+          <ul>
+            <li>Finished reading about threads</li>
+            <li>Div2 Codeforces contest</li>
+          </ul>
         </TimelineParagraph>
       </TimelineComponent>
       <TimelineMiddle>
         <TimelinePoint />
       </TimelineMiddle>
       <TimelineComponent>
-        <TimelineDate>May 25, 2024</TimelineDate>
+        <TimelineDate>July 18, 2024</TimelineDate>
       </TimelineComponent>
       <TimelineComponent>
-        <TimelineDate right>May 3, 2024</TimelineDate>
+        <TimelineDate right>July 16, 2024</TimelineDate>
       </TimelineComponent>
       <TimelineMiddle>
         <TimelinePoint />
       </TimelineMiddle>
       <TimelineComponent bg>
-        <TimelineTitle>First Live Stream</TimelineTitle>
-        <TimelineParagraph>In my first ever live stream, I couldn't put my cup of coffee down.</TimelineParagraph>
+        <TimelineTitle>Processes</TimelineTitle>
+        <TimelineParagraph>
+        <ul>
+            <li>Read about processes and how thry function</li>
+            <li>Practiced some calculus problems</li>
+            <li>CSES Problemset</li>
+          </ul>
+        </TimelineParagraph>
       </TimelineComponent>
       <TimelineComponent bg>
-        <TimelineTitle>Vertical Timeline</TimelineTitle>
-        <TimelineParagraph>Published this video.</TimelineParagraph>
+        <TimelineTitle>Finished making this website</TimelineTitle>
+        <TimelineParagraph>Pushed the code to GitHub</TimelineParagraph>
       </TimelineComponent>
       <TimelineMiddle>
         <TimelinePoint />
         <TimelinePoint bottom />
       </TimelineMiddle>
       <TimelineComponent>
-        <TimelineDate>April 28, 2024</TimelineDate>
+        <TimelineDate>June 16, 2024</TimelineDate>
       </TimelineComponent>
     </TimelineWrapper>
   );
